25487524e5d99ea29c249f15cd0c35ec7b4895cd
Fahad Saeed <fahadsaeed237@gmail.com>
2022-10-17 22:35:10 +0500

    usedispatch and useselector used

--- src/app/layout/auth/login/Login.tsx
+++ src/app/layout/auth/login/Login.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import {useDispatch} from "react-redux";
+import {doLogIn} from "../../../../features/auth/authSlice";
 
  const Login = () => {
+     const dispatch = useDispatch()
+     const login = () =>{
+         dispatch(doLogIn({user: 'Fahad saeed', email: 'fahadsaeed@gmail.com'}))
+     }
+
     return (
         <div className="App-Login">
             Login goes here
+            <div>
+            <button onClick={login}>Login</button>
+            </div>
         </div>
     )
 }
--- src/app/layout/dashboard/Dashboard.tsx
+++ src/app/layout/dashboard/Dashboard.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import {useDispatch, useSelector} from "react-redux";
+import {doLogout} from "../../../features/auth/authSlice";
+
 
  const Dashboard = () => {
+    const dispatch = useDispatch()
+   const auth =  useSelector((state: any) => state.auth);
+   const logout = () =>{
+       dispatch(doLogout())
+   }
+   console.log('auth', auth);
     return (
         <div className="App-Dashboard">
-            Welcome
+            Welcome {auth?.user?.user}
+            <div>
+                <button onClick={logout}>Logout</button>
+            </div>
         </div>
     )
 }
